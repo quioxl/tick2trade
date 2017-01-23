f2c1ebbfae2abe66ac73da3699e3cc6c24712bf6
Michael Horn <mike_horn@mentor.com>
2017-01-23 15:41:59 -0700

    Added typedefs to ease stimulus creation

--- sv/host_agent/host_agent_pkg.sv
+++ sv/host_agent/host_agent_pkg.sv
@@ -8,11 +8,29 @@
 
 package host_agent_pkg;
 
-   import uvm_pkg::*;
-
-   //Typedef used by the host_agent
-   typedef enum bit[1:0]  {READ, WRITE, PAUSE} instruction_type;
-
+  import uvm_pkg::*;
+  
+  //Typedefs used by the host_agent
+  typedef enum bit[7:0] {INIT_MEM, SYM_DEL, SYM_0, SYM_1} access_type;
+  //These structs enable easier construction of data when constructing a symbol
+  // portion 0 or a symbol portion 1 message in a sequence
+  typedef struct packed {
+    bit [15:0]   symbol;
+    access_type  acc_type;
+    bit [39:0]   unused0;
+    bit [31:0]   min_vol;
+    bit [31:0]   max_vol;
+    bit [63:0]   min_price;
+    bit [63:0]   max_price;
+  } data_sym_0_t;
+  
+  typedef struct packed {
+    bit [15:0]   symbol;
+    access_type  acc_type;
+    bit [103:0]  unused0;
+    bit [127:0]  order_info;
+  } data_sym_1_t;
+  
    //Include the sequence_items (transactions)
 `include "host_item.svh"
 
