9a105e8e41e4372b6438e017db027175c0269062
Michael Horn <mike_horn@mentor.com>
2017-01-25 14:19:34 -0700

    Instantiated tts

--- rtl/design.f
+++ rtl/design.f
@@ -1,1 +1,4 @@
 $TICK2TRADE_ROOT_DIR/rtl/feed/feed_decoder.sv
+$TICK2TRADE_ROOT_DIR/rtl/strategy/rcb.sv
+$TICK2TRADE_ROOT_DIR/rtl/strategy/tts.sv
+
--- tb/strategy_tb/tb.sv
+++ tb/strategy_tb/tb.sv
@@ -28,32 +28,11 @@ module tb;
   assign host_if.clk = clk;
   assign host_if.reset_n = reset_n;
 
-  // DUT Instantiation
-/* -----\/----- EXCLUDED -----\/-----
-  strategy #(.C_PKT_BEAT_BYTES(8),
-                 .C_PKT_MAX_BYTES(1500),
-                 .C_MSG_CNT_BYTES(2),
-                 .C_MSG_LEN_BYTES(2),
-                 .C_MSG_MIN_BYTES(8),
-                 .C_MSG_MAX_BYTES(32)) DUT (
-    .clk                ( clk                         ),
-    .reset_n            ( reset_n                     ),
-    .in_ready           ( avl_master_if.ready         ),
-    .in_valid           ( avl_master_if.valid         ),
-    .in_startofpacket   ( avl_master_if.startofpacket ),
-    .in_endofpacket     ( avl_master_if.endofpacket   ),
-    .in_data            ( avl_master_if.data          ),
-    .in_empty           ( avl_master_if.empty         ),
-    .in_error           ( avl_master_if.error         ),
-    .out_ready          ( avl_slave_if.ready          ),
-    .out_valid          ( avl_slave_if.valid          ),
-    .out_startofpacket  ( avl_slave_if.startofpacket  ),
-    .out_endofpacket    ( avl_slave_if.endofpacket    ),
-    .out_data           ( avl_slave_if.data           ),
-    .out_empty          ( avl_slave_if.empty          ),
-    .out_error          ( avl_slave_if.error          )
-  );
- -----/\----- EXCLUDED -----/\----- */
+  // DUT Instantiation (Using default parameters)
+  tts DUT (
+           .feed_if (avl_master_if),
+           .host_if (host_if)
+           );
 
   initial begin
     uvm_config_db#(virtual avalon_if)::set(null,"*","avl_master_if",avl_master_if);
