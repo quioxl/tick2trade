190582d0625657e0eded7045a4b02d4e7372927c
rstuber <rstuber@gmail.com>
2017-01-26 11:34:12 -0800

    updated cmp and added new pkg structs, connected everything in tts

--- rtl/top/t2t_pkg.sv
+++ rtl/top/t2t_pkg.sv
@@ -7,6 +7,48 @@
 // ---------------------------------------------------------------------------
 package t2t_pkg;
 
+  // ------------------------------------------------------------
+  // Message elements
+  // ------------------------------------------------------------
+  // Number of bytes in each field
+  parameter MSG_TYPE_BYTES  = 3;
+  parameter MSG_SYMID_BYTES = 2;
+  parameter MSG_PRICE_BYTES = 8;
+  parameter MSG_VOL_BYTES   = 4;
+
+  // Offsets for message field
+  // First beat
+  parameter MSG_WIDTH   = 64;
+  parameter MSG_TYPE    = 40;
+  parameter MSG_SYMID   = 24;
+  // Second beat
+  parameter MSG_PRICE   = 24;
+  // Third beat
+  parameter MSG_VOL     = 56;
+  
+  typedef struct packed {
+    bit [MSG_WIDTH-1:MSG_TYPE]  msg_type;
+    bit [MSG_TYPE-1 :MSG_SYMID] symid;
+    bit [MSG_SYMID-1:0]         price_up;
+  } t_msg_beat1;
+
+  typedef struct packed {
+    bit [MSG_WIDTH-1:MSG_PRICE] price_dn;
+    bit [MSG_PRICE-1:0]         vol_up;
+  } t_msg_beat2;
+
+  typedef struct packed {
+    bit [MSG_WIDTH-1:MSG_VOL] vol_dn;
+    bit [MSG_VOL-1  :0]       unused;
+  } t_msg_beat3;
+
+  typedef union packed {
+    t_msg_beat1                 beat1;
+    t_msg_beat2                 beat2;
+    t_msg_beat3                 beat3;
+    bit         [MSG_WIDTH-1:0] raw;
+  } t_dec_msg;
+
   // Message Type encoding
   parameter  MSG_NEW           = 24'h4E_45_57; // ASCII "NEW"
 
--- sv/avalon/avalon_if.sv
+++ sv/avalon/avalon_if.sv
@@ -3,6 +3,8 @@
 //  Description: Avalon Interface
 //    // Bundle of wires (logic) to attach to either an Avalon master or slave
 // ---------------------------------------------------------------------------
+import t2t_pkg::*;
+
 interface avalon_if();
   logic        clk;
   logic        reset_n;
@@ -13,4 +15,7 @@ interface avalon_if();
   logic [63:0] data;
   logic  [2:0] empty;
   logic        error;
+  t_dec_msg    dec_data; // Uses the union to define the message fields
+
+  assign dec_data = data;
 endinterface : avalon_if
