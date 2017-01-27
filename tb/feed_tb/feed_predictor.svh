2677029d9ee8c74300c3015f110e1a9bdcebeba8
Jon Craft <jonathan_craft@mentor.com>
2017-01-27 09:44:38 -0800

    Modified data types of feed predictor output & scoreboard input

--- tb/feed_tb/feed_predictor.svh
+++ tb/feed_tb/feed_predictor.svh
@@ -1,3 +1,11 @@
+// Predictor component for use in avalon feed TLM paths
+//  Often predictors will do some sort of object manipulation or filtering on
+//  incoming object stream, but in this case all the predictor does is convert
+//  transactions of type avalon_message_item to the base class type "avalon_seq_item_base"
+
+// This allows the downstream scoreboard to be symmetrical, i.e. both sides act on
+// the base Avalon sequence item
+
 class feed_predictor extends uvm_subscriber #(avalon_message_item);
   `uvm_component_utils(feed_predictor)
 
@@ -5,8 +13,8 @@ class feed_predictor extends uvm_subscriber #(avalon_message_item);
     super.new(name,parent);
   endfunction
 
-  avalon_message_item feed_trans_h;
-  uvm_analysis_port #(avalon_message_item) ap;
+  avalon_seq_item_base base_trans_h;
+  uvm_analysis_port #(avalon_seq_item_base) ap;
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
@@ -14,9 +22,9 @@ class feed_predictor extends uvm_subscriber #(avalon_message_item);
   endfunction
 
   function void write(avalon_message_item t);
-    feed_trans_h = avalon_message_item::type_id::create("feed_trans_h");
-    feed_trans_h.copy(t);
-    ap.write(feed_trans_h);
+    base_trans_h = avalon_seq_item_base::type_id::create("feed_trans_h");
+    base_trans_h.copy(t);
+    ap.write(base_trans_h);
   endfunction
 
 endclass
