3b3d32ff64cca176314a9c809040dfb66bb5830e
Jake Wiltgen <jw888556@lvnvda0878.lvn.broadcom.net>
2017-01-26 20:25:10 -0800

    adding cover props and groups to avalon if

--- rtl/binds/avalon_if_bind.sv
+++ rtl/binds/avalon_if_bind.sv
@@ -19,6 +19,18 @@
    ERROR_``check``: assert property (@(posedge clk) disable iff (!reset_n) (pa)) else $error("%s",{`"``check``: `",msg});
 `endif
 
+`define cover_prop_default(check, pc) \
+``check``: cover property (@(posedge clk) disable iff (!reset_n) (pc));
+
+`define cover_prop_clkrst(check, pc, dc, clk) \
+``check``: cover property (@(posedge clk) disable iff (dc) (pc));
+
+`define cover_point_default(check, pc) \
+``check``: coverpoint pc iff (reset_n);
+
+`define cover_point_clkrst(check, pc, dc) \
+``check``: coverpoint pc iff (dc);
+
 interface avalon_if_bind #( parameter DATA_WIDTH = 64,
                             parameter EMPTY_WIDTH = 3) (
 
@@ -34,10 +46,14 @@ interface avalon_if_bind #( parameter DATA_WIDTH = 64,
 
  );
 
-  reg in_pkt;
+  localparam DUT_PIPELINE_DEPTH = 10;
+
+  reg       in_pkt;
+  reg [4:0] rdy_stall_cnt;
+  reg [4:0] vld_stall_cnt;
 
   // In packet identification logic
-  always @(posedge clk) begin
+  always_ff @(posedge clk) begin
     if (!reset_n) begin
       in_pkt <= 1'b0;
     end else begin
@@ -49,6 +65,43 @@ interface avalon_if_bind #( parameter DATA_WIDTH = 64,
     end
   end
 
+  // Ready Stall counter
+  always_ff @(posedge clk) begin
+    if (!reset_n) begin
+      rdy_stall_cnt <= 1'b0;
+    end else begin
+      if (in_pkt) begin
+        if (!ready) begin
+          rdy_stall_cnt <= rdy_stall_cnt +1;
+        end else begin
+          rdy_stall_cnt <= 'h0;
+        end
+      end else if (in_pkt && endofpacket) begin
+        rdy_stall_cnt <= 'h0;
+      end
+    end
+  end
+
+  // Valid Stall counter
+  always_ff @(posedge clk) begin
+    if (!reset_n) begin
+      vld_stall_cnt <= 1'b0;
+    end else begin
+      if (in_pkt) begin
+        if (!ready) begin
+          vld_stall_cnt <= vld_stall_cnt +1;
+        end else begin
+          vld_stall_cnt <= 'h0;
+        end
+      end else if (in_pkt && endofpacket) begin
+        vld_stall_cnt <= 'h0;
+      end
+    end
+  end
+
+  //------------------------------------------------------------------------------------
+  // Assertions
+  //------------------------------------------------------------------------------------
   `assert_prop_default(assert_invalid_eop,
                       (endofpacket |-> valid),
                       "EOP asserted without valid")
@@ -60,6 +113,64 @@ interface avalon_if_bind #( parameter DATA_WIDTH = 64,
   `assert_prop_default(valid_deassert,
                       (!ready |-> ##1 !valid),
                       "Valid did not de-assert the clock after read de-asserted")
+
+  `assert_prop_default(invalid_eop,
+                      (endofpacket |->  in_pkt),
+                      "EOP asserted while not in packet")
+
+  //------------------------------------------------------------------------------------
+  // Embedded coverage
+  //------------------------------------------------------------------------------------
+  covergroup cg_avalon_fields @(posedge clk);
+
+    // TITLE: Cover all states have been hit
+    cp_empty: coverpoint empty iff (reset_n) {
+      bins MIN       = {'h0                    };
+      bins MID       = {[1:(2**EMPTY_WIDTH-2)] };
+      bins MAX       = {(2**EMPTY_WIDTH-1)     };
+    }
+
+    cp_rdy_stall_cnt: coverpoint rdy_stall_cnt iff (reset_n) {
+      bins MIN       = {'h1                        };
+      bins MID       = {[2:(DUT_PIPELINE_DEPTH-1)] };
+      bins MAX       = {DUT_PIPELINE_DEPTH         };
+    }
+
+    cp_vld_stall_cnt: coverpoint vld_stall_cnt iff (reset_n) {
+      bins MIN       = {'h1                        };
+      bins MID       = {[2:(DUT_PIPELINE_DEPTH-1)] };
+      bins MAX       = {DUT_PIPELINE_DEPTH         };
+    }
+
+   endgroup: cg_avalon_fields
+   cg_avalon_fields cg_avalon_fields_inst=new;
+
+  `cover_prop_default(b2b_beats,
+              ( ready && valid |-> ##1 ready && valid))
+
+  `cover_prop_default(min_pkt_size,
+              ( startofpacket |-> ##1 endofpacket))
+
+  `cover_prop_default(single_cycle_stall_between_packets,
+              ( startofpacket |-> ##2 endofpacket))
+
+  `cover_prop_default(b2b_pkt,
+              ( endofpacket |-> ##1 startofpacket))
+
+  `cover_prop_default(b2b_min_pkt,
+              ( startofpacket |-> ##1 endofpacket |-> ##1 startofpacket |-> ##1 endofpacket))
+
+  `cover_prop_default(dest_stall_sop,
+              ( startofpacket |-> !ready))
+
+  `cover_prop_default(dest_stall_eop,
+              ( endofpacket |-> !ready))
+
+  `cover_prop_default(dest_stall_in_pkt,
+              ( in_pkt |-> !ready))
+
+  `cover_prop_default(source_stall_in_pkt,
+              ( in_pkt |-> !valid))
 
   initial begin
     $display("INFO: avalon_if_bind file loaded");
