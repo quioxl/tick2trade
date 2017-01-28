7943039ca609e7286b9c0498e62e5a9b06c7a8f4
Michael Horn <mike_horn@mentor.com>
2017-01-28 16:25:52 -0700

    Added Summary and PASS/FAIL message at end of sim

--- tests/system_tests/sanity_test.svh
+++ tests/system_tests/sanity_test.svh
@@ -12,8 +12,11 @@ class sanity_test extends system_test_base;
     
     super.run_phase(phase);
     host_seq_h = random_host_program_seq::type_id::create("host_seq_h");
+    `uvm_info("MESSAGES", $sformatf("%0d symbols will be programmed in strategy RAM", host_seq_h.trans_count), UVM_LOW)
     host_seq_h.start(env_h.host_agent_h.seqr_h);
     feed_seq_h = feed_simple_seq::type_id::create("seq_h");
+    `uvm_info("MESSAGES", $sformatf("%0d messages will be sent", feed_seq_h.trans_count + 1), UVM_LOW)
+    messege_sent_count += (feed_seq_h.trans_count + 1);
     feed_seq_h.start(env_h.layering_h.feed_message_seqr_h);
     #1us;
     phase.drop_objection(this);
--- tests/system_tests/system_test_base.svh
+++ tests/system_tests/system_test_base.svh
@@ -1,6 +1,9 @@
 class system_test_base extends uvm_test;
   `uvm_component_utils(system_test_base)
 
+  //Data Members
+  int messege_sent_count = 0;
+  
   system_env env_h;
   system_env_config cfg_h;
 
@@ -26,5 +29,16 @@ class system_test_base extends uvm_test;
     cfg_h.wait_for_reset();
     cfg_h.wait_for_clocks(10);
   endtask : run_phase
+
+  function void report_phase(uvm_phase phase);
+    string message;
+    super.report_phase(phase);
+    $sformat(message, "\n-------------------------------------\nMessages Sent In           : %0d\nMessages Discarded          : %0d\nOrders Compared Successfully: %0d\nOrders Miscompared          : %0d\n-------------------------------------\n", messege_sent_count, env_h.strat_pred_h.discarded.sum(), env_h.scoreboard_h.compare_count, env_h.scoreboard_h.miscompare_count);
+    if (messege_sent_count == (env_h.strat_pred_h.discarded.sum() + env_h.scoreboard_h.compare_count))
+      $sformat(message, "%s               PASSED\n-------------------------------------", message);
+    else
+      $sformat(message, "%s               FAILED\n-------------------------------------", message);
+    `uvm_info("TEST_STATUS", message, UVM_NONE)
+  endfunction : report_phase
 
 endclass : system_test_base
