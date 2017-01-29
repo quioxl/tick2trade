51dc6b1708915ca235f68f6dae7805f15200d00b
Michael Horn <mike_horn@mentor.com>
2017-01-28 17:10:07 -0700

    More robust summary and PASS/FAIL checking.  Also fixed incorrect sequencer usage in all_messages_test.

--- tests/system_tests/all_messages_test.svh
+++ tests/system_tests/all_messages_test.svh
@@ -20,12 +20,13 @@ class all_messages_test extends system_test_base;
     host_seq_h = random_host_program_seq::type_id::create("host_seq_h");
     //Program 100 symbols
     host_seq_h.trans_count = 100;
+    symbols_programmed = host_seq_h.trans_count;
     `uvm_info("MESSAGES", $sformatf("%0d symbols will be programmed in strategy RAM", host_seq_h.trans_count), UVM_LOW)
     host_seq_h.start(env_h.host_agent_h.seqr_h);
     feed_seq_h = feed_all_messages_seq::type_id::create("feed_seq_h");
     `uvm_info("MESSAGES", $sformatf("%0d messages will be sent", feed_seq_h.message_count), UVM_LOW)
     messege_sent_count += feed_seq_h.message_count;
-    feed_seq_h.start(env_h.master_agent_h.seqr_h);
+    feed_seq_h.start(env_h.layering_h.feed_message_seqr_h);
     #1us;
     phase.drop_objection(this);
   endtask : run_phase
--- tests/system_tests/system_test_base.svh
+++ tests/system_tests/system_test_base.svh
@@ -2,6 +2,7 @@ class system_test_base extends uvm_test;
   `uvm_component_utils(system_test_base)
 
   //Data Members
+  int symbols_programmed = 0;
   int messege_sent_count = 0;
   
   system_env env_h;
@@ -30,11 +31,22 @@ class system_test_base extends uvm_test;
     cfg_h.wait_for_clocks(10);
   endtask : run_phase
 
+    
   function void report_phase(uvm_phase phase);
     string message;
+    uvm_report_server serv = uvm_report_server::get_server();
+    
     super.report_phase(phase);
-    $sformat(message, "\n-------------------------------------\nMessages Sent In           : %0d\nMessages Discarded          : %0d\nOrders Compared Successfully: %0d\nOrders Miscompared          : %0d\n-------------------------------------\n", messege_sent_count, env_h.strat_pred_h.discarded.sum(), env_h.scoreboard_h.compare_count, env_h.scoreboard_h.miscompare_count);
-    if (messege_sent_count == (env_h.strat_pred_h.discarded.sum() + env_h.scoreboard_h.compare_count))
+    $sformat(message, "\n-------------------------------------\nSymbols Programmed  : %6d\nSymbols Reprogrammed: %6d", symbols_programmed, ((cfg_h.enable_new_order_gen) ? env_h.new_order_gen_h.symbols_reprogrammed : 0) );
+    $sformat(message, "\n-------------------------------------\nSymbols Programmed  : %6d\nSymbols Reprogrammed: %6d\n-------------------------------------\nMessages Sent In            : %6d\nMessages Discarded          : %6d\nOrders Compared Successfully: %6d\nOrders Miscompared          : %6d\n-------------------------------------\n", symbols_programmed, ((cfg_h.enable_new_order_gen) ? env_h.new_order_gen_h.symbols_reprogrammed : 0), messege_sent_count, env_h.strat_pred_h.discarded.sum(), env_h.scoreboard_h.compare_count, env_h.scoreboard_h.miscompare_count);
+    //Total messages sent should equal the total discarded + the total number
+    // of orders
+    // Additionally, the total number or reprograms should equal the total
+    // number of orders
+    if ((serv.get_severity_count(UVM_FATAL) == 0) && 
+        (serv.get_severity_count(UVM_ERROR) == 0) &&
+        (env_h.scoreboard_h.miscompare_count == 0) && (messege_sent_count == (env_h.strat_pred_h.discarded.sum() + env_h.scoreboard_h.compare_count)) &&
+        ( (cfg_h.enable_new_order_gen) ? (env_h.new_order_gen_h.symbols_reprogrammed == env_h.scoreboard_h.compare_count) : 1) )
       $sformat(message, "%s               PASSED\n-------------------------------------", message);
     else
       $sformat(message, "%s               FAILED\n-------------------------------------", message);
