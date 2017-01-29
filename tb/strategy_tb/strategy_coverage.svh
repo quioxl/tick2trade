8bca69a4c402823d9088da136f08a2aa2161813a
Michael Horn <mike_horn@mentor.com>
2017-01-29 10:40:40 -0700

    Updated feed coverage, added host and order coverage.

--- tb/strategy_tb/strategy_coverage.svh
+++ tb/strategy_tb/strategy_coverage.svh
@@ -8,7 +8,7 @@ class strategy_coverage extends uvm_component;
 
   //Local handles for sampling
   avalon_seq_item_base feed_item_h;
-  host_item            host_item_h;
+  host_trans_t         host_struct;
   order_item           order_item_h;
   
   //Covergroups
@@ -26,11 +26,37 @@ class strategy_coverage extends uvm_component;
       }
     SIZE_TYPE: cross SIZE, TYPE;
   endgroup : feed_items
+
+  covergroup host_items;
+    CMDS: coverpoint host_struct.cmd;
+    RAMS: coverpoint host_struct.ram;
+    ADDR: coverpoint host_struct.addr iff (host_struct.ram != SYMBOL)
+      //Only for not symbol because that is when the RAM address is on the line
+      {
+       bins low_range            = {[16'h0000:16'h0fff]};
+       bins medium_range         = {[16'h1000:16'h2fff]};
+       bins high_range           = {[16'h3000:16'h3fff]};
+       illegal_bins out_of_range = {[16'h4000:$]};
+      }
+  endgroup : host_items
+
+  covergroup order_items;
+    SYMBOLS: coverpoint order_item_h.data[127:112]
+      {
+       bins low_range            = {[16'h0000:16'h3fff]};
+       bins low_med_range        = {[16'h4000:16'h7fff]};
+       bins med_high_range       = {[16'h8000:16'hbfff]};
+       bins high_range           = {[16'hc000:16'hffff]};
+      }
+  endgroup : order_items
+  
   
   //Methods
   function new(string name, uvm_component parent);
     super.new(name,parent);
     feed_items = new();
+    host_items = new();
+    order_items = new();
   endfunction : new
 
   function void build_phase(uvm_phase phase);
@@ -46,11 +72,13 @@ class strategy_coverage extends uvm_component;
   endfunction : write_feed
   
   function void write_host(host_item t);
-    host_item_h = t;
+    host_struct = t.data;
+    host_items.sample();
   endfunction : write_host
   
   function void write_order(order_item t);
     order_item_h = t;
+    order_items.sample();
   endfunction : write_order
   
 endclass : strategy_coverage
